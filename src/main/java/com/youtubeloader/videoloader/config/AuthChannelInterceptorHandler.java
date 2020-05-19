683758ce3c9b2a26ba8b43a783e97fe3293f9bfe
Agaev Artem <Agaev.A.R@sberbank.ru>
2020-05-19 22:20:44 +0300

    + security completed

--- src/main/java/com/youtubeloader/videoloader/config/AuthChannelInterceptorHandler.java
+++ src/main/java/com/youtubeloader/videoloader/config/AuthChannelInterceptorHandler.java
@@ -3,7 +3,11 @@ package com.youtubeloader.videoloader.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,6 +20,16 @@ public class AuthChannelInterceptorHandler implements ChannelInterceptor {
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        final StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+        if (StompCommand.CONNECT == accessor.getCommand()) {
+            final String username = accessor.getFirstNativeHeader(USERNAME_HEADER);
+            final String password = accessor.getFirstNativeHeader(PASSWORD_HEADER);
+
+            final UsernamePasswordAuthenticationToken user = webSocketAuthenticatorService.getAuthenticationOrFail(username, password);
+
+            accessor.setUser(user);
+        }
         return message;
     }
 }
